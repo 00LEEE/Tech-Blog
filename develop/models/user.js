9f8076ec619c9e3c0d72c3d31be8ba1f527aad40
Kole Dallam <kolejasonD@gmail.com>
2023-05-18 16:04:43 -0600

    styling for more of a contemporary look

---
+++ develop/models/user.js
@@ -0,0 +1,45 @@
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+const Sequelize = require('sequelize');
+const { Model, DataTypes } = Sequelize;
+
+class User extends Model {
+  async checkPassword(userPassword) {
+    return await bcrypt.compare(userPassword, this.password);
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8, 50],
+      },
+    },
+  },
+  {
+    hooks: {
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
+    sequelize,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
